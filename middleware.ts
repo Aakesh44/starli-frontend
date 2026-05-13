4ad2ed5657e81da314b1523e8e527bc7f030408d
Aakesh44 <aakeshviswanathan@gmail.com>
2026-05-13 23:08:06 +0530

    final push

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,39 @@
-import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+// import { withAuth } from 'next-auth/middleware';
+// import { NextResponse } from 'next/server';
 
-const publicRoutes = [
-    '/login',
-    '/signup',
-    '/email-verify',
-    '/forgot-password',
-    '/reset-password',
-];
+// const publicRoutes = [
+//     '/login',
+//     '/signup',
+//     '/email-verify',
+//     '/forgot-password',
+//     '/reset-password',
+// ];
 
-export default withAuth({
-    pages: {
-        signIn: '/login',
-    },
-    callbacks: {
-        authorized: ({ token, req }) => {
-            const pathname = req.nextUrl.pathname;
+// export default withAuth({
+//     pages: {
+//         signIn: '/login',
+//     },
+//     callbacks: {
+//         authorized: ({ token, req }) => {
+//             const pathname = req.nextUrl.pathname;
 
-            // Allow public routes
-            const isPublicRoute = publicRoutes.some(route =>
-                pathname.startsWith(route)
-            );
+//             // Allow public routes
+//             const isPublicRoute = publicRoutes.some(route =>
+//                 pathname.startsWith(route)
+//             );
 
-            if (isPublicRoute) {
-                return true;
-            }
+//             if (isPublicRoute) {
+//                 return true;
+//             }
 
-            // Protect everything else
-            return !!token;
-        },
-    },
-});
+//             // Protect everything else
+//             return !!token;
+//         },
+//     },
+// });
 
-export const config = {
-    matcher: [
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    ],
-};
+// export const config = {
+//     matcher: [
+//         '/((?!api|_next/static|_next/image|favicon.ico).*)',
+//     ],
+// };
