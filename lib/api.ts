3a1e07126d8d6ff80c9e2e25e29dae153a7cc3b8
Aakesh44 <aakeshviswanathan@gmail.com>
2026-05-13 21:33:38 +0530

    final push

--- lib/api.ts
+++ lib/api.ts
@@ -11,7 +11,6 @@ const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
     },
     withCredentials: true,
     timeout: 60000,
